a033dcaea05042e3e315d6d7fc6a885b9ba3b8d0
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-11-22 17:07:22 +0000

    Find tests written and passing

--- spec/recipe_repository_spec.rb
+++ spec/recipe_repository_spec.rb
@@ -29,5 +29,25 @@ RSpec.describe RecipeRepository do
       expect(recipes[1].cooking_time).to eq 20
       expect(recipes[1].rating).to eq 4
     end
+
+    it "returns the bread recipe when finding id 1" do
+      repo = RecipeRepository.new
+
+      recipe = repo.find(1)
+      expect(recipe.id).to eq '1'
+      expect(recipe.name).to eq 'bread'
+      expect(recipe.cooking_time).to eq 60
+      expect(recipe.rating).to eq 3
+    end
+
+    it "returns the pancake recipe when finding id 2" do
+      repo = RecipeRepository.new
+      
+      recipe = repo.find(2)
+      expect(recipe.id).to eq '2'
+      expect(recipe.name).to eq 'pancakes'
+      expect(recipe.cooking_time).to eq 20
+      expect(recipe.rating).to eq 4
+    end
   end
 end
